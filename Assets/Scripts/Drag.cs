a34167e001f8d368041e79e270a37960948a8264
iamttp <1877784332@qq.com>
2020-12-01 14:09:32 +0800

    complete scene and need add drag logic

---
+++ Assets/Scripts/Drag.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drag : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+
+    void OnMouseDown()
+    {
+
+    }
+}
