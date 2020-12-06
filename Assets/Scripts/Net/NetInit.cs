a8377e4e0fde87577d02cf9ca9264ee42fa10cfe
iamttp <1877784332@qq.com>
2020-12-06 19:08:07 +0800

    initial base net module

---
+++ Assets/Scripts/Net/NetInit.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using UnityEngine;
+
+// 小心Socket内存泄漏
+public class NetInit : MonoBehaviour
+{
+    void Start()
+    {
+        startConnect();
+        StartCoroutine(Ping(0));
+    }
+
+    void Update()
+    {
+
+    }
+
+    Socket socket;
+    private void startConnect()
+    {
+        try
+        {
+            int _port = 8999;
+            string _ip = "39.97.171.148";
+
+            //创建客户端Socket，获得远程ip和端口号
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPAddress ip = IPAddress.Parse(_ip);
+            IPEndPoint point = new IPEndPoint(ip, _port);
+
+            socket.Connect(point);
+            Debug.Log("连接成功!");
+            //开启新的线程，不停的接收服务器发来的消息
+            Thread c_thread = new Thread(Received);
+            c_thread.IsBackground = true;
+            c_thread.Start();
+        }
+        catch (System.Exception)
+        {
+            Debug.Log("IP或者端口号错误...，服务器程序未启动");
+            socket.Close();
+            socket.Dispose();
+            return;
+        }
+    }
+
+    /// <summary>
+    /// 接收服务端返回的消息
+    /// </summary>
+    byte[] receiveBuf = new byte[1024];
+    private void Received()
+    {
+        while (true)
+        {
+            try
+            {
+                //实际接收到的有效字节数
+                int len = socket.Receive(receiveBuf);
+                if (len == 0) break;
+                Message msg = PackUtil.Unpack(receiveBuf);
+                Debug.Log(msg);
+            }
+            catch
+            {
+                Debug.Log("接收数据失败");
+                socket.Close();
+                socket.Dispose();
+                return;
+            }
+        }
+    }
+
+    /// <summary>
+    /// 向服务器发送消息
+    /// </summary>
+    private void Send(Message msg)
+    {
+        byte[] sendBuf = PackUtil.Pack(msg);
+        socket.Send(sendBuf);
+    }
+
+    private IEnumerator Ping(uint id)
+    {
+        yield return new WaitForSeconds(1);
+        try
+        {
+            Send(new Message(id, new byte[] { 1, 2, 3 }));
+        }
+        catch
+        {
+            Debug.Log("数据发送失败");
+            socket.Close();
+            socket.Dispose();
+            yield break;
+        }
+        if (id == 0) StartCoroutine(Ping(1));
+        if (id == 1) StartCoroutine(Ping(0));
+    }
+}
---
+++ Assets/Scripts/Net/PackUtil.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public class Message
+{
+    public uint len;
+    public uint id;
+    public byte[] data;
+
+    public Message(uint id, byte[] data)
+    {
+        this.id = id;
+        this.data = data;
+        len = (uint)data.Length;
+    }
+
+    public override string ToString()
+    {
+        return len.ToString() + ", " + id.ToString() + ", " + BitConverter.ToString(data);
+    }
+}
+
+// TODO 性能优化
+public class PackUtil
+{
+    public static Message Unpack(byte[] dataPack)
+    {
+        byte[] arr = new byte[4];
+        Array.Copy(dataPack, 0, arr, 0, 4);
+        uint len = BitConverter.ToUInt32(arr, 0);
+        Array.Copy(dataPack, 4, arr, 0, 4);
+        uint id = BitConverter.ToUInt32(arr, 0);
+
+        byte[] data = new byte[len];
+        // len id 4 + 4 = 8 字节
+        Array.Copy(dataPack, 8, data, 0, len);
+        return new Message(id, data);
+    }
+
+    public static byte[] Pack(Message msg)
+    {
+        //if (BitConverter.IsLittleEndian) UnityEngine.Debug.Log("小端");
+        //else UnityEngine.Debug.Log("大端");
+
+        var d1 = BitConverter.GetBytes(msg.len);
+        var d2 = BitConverter.GetBytes(msg.id);
+        var d3 = msg.data;
+
+        var res = new byte[d1.Length + d2.Length + d3.Length];
+        d1.CopyTo(res, 0);
+        d2.CopyTo(res, d1.Length);
+        d3.CopyTo(res, d1.Length + d2.Length);
+        return res;
+    }
+}
